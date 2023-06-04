894996ae7e98d4130790b26cada821254e8a6d6a
Harshithreddy2002 <130286953+Harshithreddy2002@users.noreply.github.com>
2023-06-04 12:16:35 +0530

    Update Shortener.py

--- Shortener.py
+++ Shortener.py
@@ -1,10 +1,40 @@
+from copy import copy
+from struct import pack
+from tokenize import String
+import pyperclip
 import pyshorteners
+from tkinter import *
 import validators
-url=input("Enter a link: ")
-def shorten_url(url):
-    s=pyshorteners.Shortener()
-    if not validators.url(url):
-        print('Url is not valid')
+
+#GUI
+root = Tk()
+root.geometry("700x350")
+root.title("URL Shortner")
+root.configure(bg="#7F7FFF")
+
+#Define Variable for url
+urlmain = StringVar()
+urlshortmain = StringVar()
+
+
+#Define Function
+def urlShortner():
+    urladdress = urlmain.get()
+    urlshort = pyshorteners.Shortener().tinyurl.short(urladdress)
+    if not validators.url(urladdress):
+        urlshortmain.set('URL is not valid')
     else:
-        print(s.tinyurl.short(url))
-shorten_url(url)
+        urlshortmain.set(urlshort)
+
+def copyurl():
+    urlshort = urlshortmain.get()
+    pyperclip.copy(urlshort)
+
+
+Label(root,text="URL Shortner",font=('Arial', 12)).pack(pady=10)
+Entry(root,textvariable=urlmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
+Button(root,text="Generate Short Url",command=urlShortner).pack(pady=7)
+Entry(root,textvariable=urlshortmain,width=50,font=('Arial 14')).pack(padx=10, pady=10)
+Button(root,text="Copy Short Url",command=copyurl).pack(pady=5)
+
+root.mainloop()
